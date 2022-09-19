f99946044f52f23fd31211c73a48e4a26bf6e64f
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2022-09-19 23:25:58 +0300

    add main page

---
+++ img/social_stylesheet.sql
@@ -0,0 +1,29 @@
+.sprite {
+    background-image: url(spritesheet.png);
+    background-repeat: no-repeat;
+    display: block;
+}
+
+.sprite-Vectorface {
+    width: 9px;
+    height: 16px;
+    background-position: -5px -5px;
+}
+
+.sprite-Vectorinst {
+    width: 16px;
+    height: 16px;
+    background-position: -24px -5px;
+}
+
+.sprite-Vectortick {
+    width: 14px;
+    height: 16px;
+    background-position: -50px -5px;
+}
+
+.sprite-Vectortube {
+    width: 20px;
+    height: 14px;
+    background-position: -74px -5px;
+}
